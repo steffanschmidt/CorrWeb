8a97c279886523a9abfc80d62e54d2b5d8d22e1b
steffanschmidt <39085282+steffanschmidt@users.noreply.github.com>
2019-03-10 10:27:00 +0100

    WebsiteInit

---
+++ MyScripts/ActiveLink.js
@@ -0,0 +1,10 @@
+﻿function findActiveLink() {
+    var currentUrl = window.location.href;
+    $(".navigationalcontainer a").each(function () {
+
+        if (currentUrl === this.href) {
+            $(this).closest("a").addClass("activeLink");
+        }
+
+    });
+}
