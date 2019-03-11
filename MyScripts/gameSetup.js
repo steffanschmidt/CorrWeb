bddc51cf5ede457f92b4e813b56f00c63a21cbba
steffanschmidt <39085282+steffanschmidt@users.noreply.github.com>
2019-03-11 23:02:16 +0100

    Updates
    - Programming
    - Game

--- MyScripts/gameSetup.js
+++ MyScripts/gameSetup.js
@@ -5,7 +5,6 @@
     for (let i = 0; i < buttons.length; i++) {
         $("#" + buttons[i]).bind("click", button_callback[i]);
     }
-
     sessionStorage.setItem("startedGame", 0);
     startBtnState();
 
@@ -14,18 +13,25 @@
 // Game Canvas
 var gameCanvas = document.getElementById("game");
 ctx = gameCanvas.getContext("2d");
+var devicePixelRatio = window.devicePixelRatio;
+var bsr = ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    ctx.backingStorePixelRatio || 1;
+var scale = devicePixelRatio / bsr;
+
+// Ensuring Scale is in order -> avoids blurry text and small images
+gameCanvas.width = 850 * scale;
+gameCanvas.height = 458 * scale;
+gameCanvas.style.width = 850 + "px";
+gameCanvas.style.height = 458 + "px";
+ctx.setTransform(scale, 0, 0, scale, 0, 0);
 
 // constants
 var gravity = 1;
 var jumpHeight = 20;
 var moveLength = 3;
-
-var playerPosXInit = gameCanvas.width / 2;
-var playerPosYInit = gameCanvas.height;
-
-drawDoorWays();
-
-document.addEventListener("keydown", gameInput, false);
 
 function gameInput(event) {
     switch (event.keyCode) {
@@ -49,6 +55,7 @@ function gameInput(event) {
 
 function startBtnState() {
 
+    
     var startBtn = document.getElementById("startGame");
     var quitBtn = document.getElementById("stopGame");
     var resetPlayerBtn = document.getElementById("resetPlayer");
@@ -71,26 +78,66 @@ function constructPlatform() {
 }
 
 function drawDoorWays() {
-    var image = document.createElement("img");
-    image.src = "../GameArt/DoorGame.png";
-    image.onload = function () {
-        ctx.drawImage(image, 0, 0);
+    var doorPositions = [];
+    var doorImg = document.createElement("img");
+    var platformImg = document.createElement("img");
+    doorImg.src = "../GameArt/DoorGame2.png";
+    platformImg.src = "../GameArt/DoorPlatform.png";
+
+    doorImg.onload = function () {
+        platformImg.onload = function () {
+            var doorText = ["About Me", "Core Competencies", "Nanoscience", "Chemistry", "Materials Science", "Practical Work", "Programming"];
+            var imgHeight = doorImg.height;
+            var imgWidth = doorImg.width;
+            var amountOfDoorsTop = 3;
+            var amountofDoorsBottom = 4;
+            var movingThingsUp = 15;
+            var spaceX = (gameCanvas.width - 4 * imgWidth) / 5;
+            var spaceY = (gameCanvas.height - 2 * imgHeight) / 3;
+            var textDist;
+
+            for (let i = 1; i < amountOfDoorsTop + 1; i++) {
+                doorPositions.push([(i + 0.5) * spaceX + ( i - 0.5 ) * imgWidth, spaceY]); // coordinates for top doors
+            }
+
+            for (let i = 1; i < amountofDoorsBottom + 1; i++) {
+                doorPositions.push([i * spaceX + (i - 1) * imgWidth, 2 * spaceY + imgHeight]); // coordinates for bottom doors
+            }
+
+            for (let i = 0; i < doorPositions.length; i++) {
+                ctx.drawImage(doorImg, doorPositions[i][0], doorPositions[i][1] - movingThingsUp); // doors
+                ctx.drawImage(platformImg, doorPositions[i][0] - 5, doorPositions[i][1] + imgHeight - movingThingsUp); // platforms
+                ctx.font = "bold 16px Arial";
+                var doorTextWidth = ctx.measureText(doorText[i]).width;
+                textDist = (imgWidth - doorTextWidth) / 2;
+                ctx.fillStyle = "white";
+                ctx.fillRect(doorPositions[i][0] + textDist - 5, doorPositions[i][1] - 50, doorTextWidth + 10, 25);
+                ctx.fillStyle = "black";
+                ctx.fillText(doorText[i], doorPositions[i][0] + textDist, doorPositions[i][1] - 30);
+            }
+        };
+
     };
-    
-    
+
 }
 
+
 function initiateGame() {
-    alert("LET ME IN!");
+    document.addEventListener("keydown", gameInput, false);
     sessionStorage.setItem("startedGame", 1);
     startBtnState();
     initiateMap();
+    initiatePlayer();
+}
 
-    
+function initiatePlayer() {
+    var playerPosXInit = gameCanvas.width / 2;
+    var playerPosYInit = gameCanvas.height;
+    alert("Play Initiated");
 }
 
 function initiateMap() {
-    alert("Initiated Map");
+    drawDoorWays();
 }
 
 function resetPlayerPosition() {
